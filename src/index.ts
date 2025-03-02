154d06be923ab72133dc32b13491c0e870b57e43
840 <code840@outlook.com>
2025-03-02 05:25:43 +0000

    匯出一些 client 的 type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 export { APIv1 } from "@/core/api/v1"
 export { APIv2 } from "@/core/api/v2"
 export { Client } from "@/core/client"
+export type {
+  AuthorizationCodeToken,
+  ClientCredentialsToken,
+  GrantType,
+} from "@/core/client"
 export { GameMode, ApprovedType, Language, Genre, Mods } from "@/objects/v1/osu"
